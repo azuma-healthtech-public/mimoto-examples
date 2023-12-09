c1d89849f5b9546fa6b36c1ec8c560ca5dfb9577
ewachtelazuma <116568777+ewachtelazuma@users.noreply.github.com>
2023-12-09 02:42:36 +0100

    feat: ios initial version - still work in progress

--- react-native-app-auth-ce/App.tsx
+++ react-native-app-auth-ce/App.tsx
@@ -43,6 +43,7 @@ const configMimoto: AuthConfiguration = {
 
 const configMimotoGematikSimulation: AuthConfiguration = {
   issuer: 'https://mimoto-test.pie.azuma-health.tech',
+  //clientId: 'b664b9ab-1484-4228-b546-7b173a860f44',
   clientId: 'da7c5825-694a-4918-85f4-e5ad1a9247db',
   redirectUrl: 'https://mimoto-example-app.azuma-health.tech/app/ce',
   additionalParameters: {provider: 'https://gsi.dev.gematik.solutions'},
@@ -52,6 +53,7 @@ const configMimotoGematikSimulation: AuthConfiguration = {
       'https://mimoto-test.pie.azuma-health.tech/connect/auth',
     tokenEndpoint: 'https://mimoto-test.pie.azuma-health.tech/connect/token',
   },
+  iosCustomBrowser: 'safari'
 };
 
 function App(): JSX.Element {
@@ -67,8 +69,37 @@ function App(): JSX.Element {
         ...config,
         connectionTimeoutSeconds: 5,
         iosPrefersEphemeralSession: true,
-        usePKCE: true,
+        skipCodeExchange: false,
+
+        usePKCE: true
       });
+
+      if( newAuthState.authorizationCode && newAuthState.authorizationCode.length != 0 ){
+        // code exchange
+        let response = await fetch(
+          "https://mimoto-test.pie.azuma-health.tech/connect/token",
+          {
+            method: 'POST', 
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+              'grant_type': 'authorization_code',
+              'client_id': config.clientId,
+              'redirect_uri': config.redirectUrl,
+              'code': newAuthState.authorizationCode,
+              'code_verifier': newAuthState.codeVerifier!
+          }).toString()
+          }
+        );
+        let responseJson = await response.json();
+        if (responseJson.error) {
+          console.error('error ', responseJson.error); // ...
+        }
+        newAuthState.accessToken = responseJson.access_token;
+        newAuthState.idToken = responseJson.id_token;
+      }
 
       const decoded = jwtDecode(newAuthState.idToken);
       setAuthState({
@@ -84,7 +115,7 @@ function App(): JSX.Element {
       // HINT: here you could also exchange mimoto AT/ID for your own AT/ID
     } catch (error) {
       // FIXME: provide proper error handling
-      Alert.alert('Failed to log in', error.message);
+      Alert.alert('Failed to log in', error?.message);
     }
   }, []);
 
--- react-native-app-auth-ce/ios/AppAuthCodeExchange/AppDelegate.mm
+++ react-native-app-auth-ce/ios/AppAuthCodeExchange/AppDelegate.mm
@@ -10,8 +10,88 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (BOOL) application: (UIApplication *)application
+             openURL: (NSURL *)url
+             options: (NSDictionary<UIApplicationOpenURLOptionsKey, id> *) options
+{
+  if ([self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:url]) {
+    return YES;
+  }
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL) application: (UIApplication *) application
+continueUserActivity: (nonnull NSUserActivity *)userActivity
+  restorationHandler: (nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+  // if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb]) {
+  
+  NSString* path = userActivity.webpageURL.path;
+  if([path isEqual: @"/code/ce"]){
+    
+    // code exchange
+    NSLog(@"Code exchange");
+    NSLog(@"%@", userActivity.webpageURL.absoluteString);
+    
+    // FIXME: start screen with progress
+    
+    NSString *url = @"https://mimoto-test.pie.azuma-health.tech/oidcf/exchange/mobile";
+    NSDictionary *jsonBodyDict = @{@"redirectUrl":userActivity.webpageURL.absoluteString, @"clientId":@"b664b9ab-1484-4228-b546-7b173a860f44"};
+    NSData *jsonBodyData = [NSJSONSerialization dataWithJSONObject:jsonBodyDict options:kNilOptions error:nil];
+    // watch out: error is nil here, but you never do that in production code. Do proper checks!
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest new];
+    request.HTTPMethod = @"POST";
+    [request setURL:[NSURL URLWithString:url]];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setHTTPBody:jsonBodyData];
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data,
+                                                                NSURLResponse * _Nullable response,
+                                                                NSError * _Nullable error) {
+      NSLog(@"Exchange finished");
+      
+      NSHTTPURLResponse *asHTTPResponse = (NSHTTPURLResponse *) response;
+      NSLog(@"The response is: %@", asHTTPResponse);
+      
+      NSDictionary *forJSONObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+      NSString* redirectUrl = [forJSONObject valueForKey:@"redirectUrl"];
+      
+      NSLog(@"Redirect url");
+      NSLog(@"%@", redirectUrl);
+      
+      // app auth
+      if (self.authorizationFlowManagerDelegate) {
+        [self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:[NSURL URLWithString:redirectUrl]];
+      }
+      
+    }];
+    [task resume];
+    
+    return YES;
+  }
+  
+  // app auth
+  if([path isEqual: @"/app/ce"]){
+    if (self.authorizationFlowManagerDelegate) {
+      NSLog(@"%@", userActivity.webpageURL.absoluteString);
+      BOOL resumableAuth = [self.authorizationFlowManagerDelegate resumeExternalUserAgentFlowWithURL:userActivity.webpageURL];
+      if (resumableAuth) {
+        return YES;
+      }
+    }
+  }
+  
+  //}
+  return [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge

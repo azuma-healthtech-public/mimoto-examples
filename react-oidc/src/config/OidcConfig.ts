7f97a1093fb2fa72f4803b2b4113bda095e1e320
ewachtelazuma <eugen.wachtel@azuma.health>
2024-07-27 15:37:32 +0200

    feat: added RU demos

--- react-oidc/src/config/OidcConfig.ts
+++ react-oidc/src/config/OidcConfig.ts
@@ -16,73 +16,91 @@ export interface ExampleOidcConfig {
     supportLoginParams: boolean
 }
 
-const autority: string = "https://mimoto-test.pie.azuma-health.tech";
+const autorityTU: string = "https://mimoto-test.pie.azuma-health.tech";
+const autorityRU: string = "https://mimoto-ref.pie.azuma-health.tech";
 const scope: string =
     "urn:telematik:alter urn:telematik:display_name urn:telematik:email urn:telematik:geschlecht urn:telematik:geburtsdatum urn:telematik:given_name urn:telematik:versicherter openid";
 const baseHostUrl = process.env.REACT_APP_HOST_URL;
 
 export const exampleConfigs: ExampleOidcConfig[] = [
     {
-        name: "Live: OIDC PKCE - Layout 1",
+        name: "(RU) Live: OIDC PKCE - Layout 1",
+        path: "oidc-live-ru",
+        description: "OIDC flow using PKCE",
+        redirect_uri: `${baseHostUrl}/oidc/signin/oidc-live-ru`,
+        authority: autorityRU,
+        client_id: "df80c822-415d-47e3-81c5-0ebb45ed0734",
+        scope: scope,
+        supportLoginParams: true
+    }, {
+        name: "(RU) Live: OIDC PKCE - Layout 2",
+        path: "oidc-live-layout-2-ru",
+        description: "OIDC flow using PKCE",
+        redirect_uri: `${baseHostUrl}/oidc/signin/oidc-live-layout-2-ru`,
+        authority: autorityRU,
+        client_id: "c28f085a-f1bf-4102-9e60-fe3703361dd4",
+        scope: scope,
+        supportLoginParams: true
+    }, {
+        name: "(RU) Live: OIDC PKCE - Web-App (mock Authenticator)",
+        path: "oidc-live-web-app-ru",
+        description: "OIDC flow using PKCE",
+        redirect_uri: `${baseHostUrl}/oidc/signin/oidc-live-web-app-ru`,
+        authority: autorityRU,
+        client_id: "5181bcde-6418-449e-ae7a-24cb16fd5433",
+        scope: scope,
+        supportLoginParams: true
+    },
+    {
+        name: "(TU) Live: OIDC PKCE - Layout 1",
         path: "oidc-live",
         description: "OIDC flow using PKCE",
         redirect_uri: `${baseHostUrl}/oidc/signin/oidc-live`,
-        authority: autority,
+        authority: autorityTU,
         client_id: "43e0b7b9-57b8-4e37-a2c2-636bd42b8267",
         scope: scope,
         supportLoginParams: true
     }, {
-        name: "Live: OIDC PKCE - Layout 2",
+        name: "(TU) Live: OIDC PKCE - Layout 2",
         path: "oidc-live-layout-2",
         description: "OIDC flow using PKCE",
         redirect_uri: `${baseHostUrl}/oidc/signin/oidc-live-layout-2`,
-        authority: autority,
+        authority: autorityTU,
         client_id: "7fbc18c4-d561-4eae-9b12-0488becc177e",
         scope: scope,
         supportLoginParams: true
     }, {
-        name: "Live: OIDC PKCE - Web-App",
+        name: "(TU) Live: OIDC PKCE - Web-App (mock Authenticator)",
         path: "oidc-live-web-app",
         description: "OIDC flow using PKCE",
         redirect_uri: `${baseHostUrl}/oidc/signin/oidc-live-web-app`,
-        authority: autority,
+        authority: autorityTU,
         client_id: "7c29aee2-36e4-47b9-8eae-37bddbe0195e",
         scope: scope,
         supportLoginParams: true
     },
     {
-        name: "Simulation: OIDC no PKCE (NOT RECOMMENDED)",
-        path: "oidc-no-pkce",
-        description: "OIDC flow without using PKCE (NOT RECOMMENDED)",
-        redirect_uri: `${baseHostUrl}/oidc/signin/oidc-no-pkce`,
-        authority: autority,
-        client_id: "5b290635-fe7e-425e-9611-1bd15e34f22e",
-        client_secret: "6zqWcl6JKaKasd123yYmFouKKUOUPFfkNppU",
-        scope: scope,
-        supportLoginParams: true
-    },
-    {
-        name: "Simulation: OIDC PKCE",
+        name: "(TU) Simulation: OIDC PKCE",
         path: "oidc-pkce",
         description: "OIDC flow using PKCE",
         redirect_uri: `${baseHostUrl}/oidc/signin/oidc-pkce`,
-        authority: autority,
+        authority: autorityTU,
         client_id: "09e9fb25-ce31-46df-887d-4b257a3d6f54",
         scope: scope,
         supportLoginParams: true
     },
     {
-        name: "Simulation: OIDC PKCE min scopes",
+        name: "(TU) Simulation: OIDC PKCE min scopes",
         path: "oidc-min-scopes",
         description: "OIDC flow using PKCE",
         redirect_uri: `${baseHostUrl}/oidc/signin/oidc-min-scopes`,
-        authority: autority,
+        authority: autorityTU,
         client_id: "776b3865-4511-4d98-b315-7ce5b1cc6387",
         scope: "openid",
         supportLoginParams: true
     },
     {
-        name: "Simulation: OIDC via Keycloak",
+        name: "(TU) Simulation: OIDC via Keycloak",
         path: "oidc-keycloak",
         description: "OIDC flow via Keycloak",
         redirect_uri: `${baseHostUrl}/oidc/signin/oidc-keycloak`,

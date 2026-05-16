79510bdb5d9d13ced519e3a0d9c2bd8e0c383935
ewachtelazuma <eugen.wachtel@azuma.health>
2026-05-16 18:04:22 +0300

    react-native-webviev-ce-app: update to latest sdk

--- react-native-webview-ce/android/app/src/main/java/com/mimotoexample/MainActivity.kt
+++ react-native-webview-ce/android/app/src/main/java/com/mimotoexample/MainActivity.kt
@@ -4,7 +4,6 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
 
@@ -20,9 +19,4 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-
-    //react-native-screens override
-    override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(null);
-    }
 }
